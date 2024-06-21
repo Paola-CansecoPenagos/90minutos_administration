5fbb6e48e390425dcebe488c708a0b080945a9af
ShonSagoro <ramosproque@gmail.com>
2024-06-21 16:41:43 -0600

    add rabbitmq queues

---
+++ src/administration_managament/infraestructure/services/GetAllPackageRatingRequest.ts
@@ -0,0 +1,30 @@
+import { Signale } from "signale";
+import { setupRabbitMQ } from "../config/RabbitConfig";
+
+export class GetAllPackageRatingRequest {
+    private queueName: string = process.env.RABBIT_QUEUE_GET_ALL_PACKAGE_RATING_REQ_ADMINISTRATION || 'default';
+    private exchangeName: string = process.env.RABBIT_EXCHANGE_ADMINISTRATION_EXCHANGE || 'default';
+    private routingKey: string = process.env.RABBIT_ROUTING_KEY_GET_ALL_PACKAGE_RATING_REQ_ADMINISTRATION || 'default';
+
+    async receiveMessage(): Promise<void> {
+        const signale = new Signale();
+        try {
+            const channel = await setupRabbitMQ(this.queueName, this.exchangeName, this.routingKey);
+            channel.consume(this.queueName, (msg: any) => {
+                if (msg) {
+                    signale.info('Message received:', msg.content.toString());
+                    const content: any = JSON.parse(msg.content.toString());
+                    signale.info('Message content:', content);
+                    // TODO: Process the message content as needed
+                    channel.ack(msg);
+                }
+            });
+        } catch (error) {
+            signale.error('Error:', error);
+        }
+    }
+}
+
+// Instancia específica para la cola de request
+const getAllPackageRatingReqSaga = new GetAllPackageRatingRequest();
+getAllPackageRatingReqSaga.receiveMessage();
